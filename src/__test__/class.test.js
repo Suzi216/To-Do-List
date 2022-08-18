eff709c4f9d505b5b3ded10706ab44af859b2bf0
Suzi216 <66145033+Suzi216@users.noreply.github.com>
2022-08-18 15:28:25 +0300

    Testing update and clearout functions

--- src/__test__/class.test.js
+++ src/__test__/class.test.js
@@ -1,4 +1,4 @@
-import { remove, add, edit } from './mocks.js';
+import { remove, add, edit, complete, deleteAll } from './mocks.js';
 
 describe('Add/Remove tests', () => {
   test('add to list works', () => {
@@ -15,12 +15,30 @@ describe('Add/Remove tests', () => {
 });
 
 
-  test("edit list", () =>{
-    let arr = [
-      { description: 'CHANGED', complete: false, index: 0 },
-      { description: 'marsela', complete: false, index: 1 },
-    ];
-    const descr="CHANGED";
-    const index=0;
-    expect(edit(descr,index)).toEqual(arr);
-  });
+
+
+test('edit list', () => {
+  const arr = [
+    { description: 'CHANGED', complete: false, index: 0 },
+    { description: 'marsela', complete: false, index: 1 },
+  ];
+  const descr = 'CHANGED';
+  const index = 0;
+  expect(edit(descr, index)).toEqual(arr);
+});
+test('update the complete status', () => {
+  const arr = [
+    { description: 'suzana', complete: true, index: 0 },
+    { description: 'marsela', complete: false, index: 1 },
+  ];
+  document.body.innerHTML = '<input type="text" class="input">';
+  const input = document.querySelector('.input');
+  expect(complete(true, 0, input)).toEqual(arr);
+});
+
+test('clear all the true status lists', () => {
+  const arr = [
+    { description: 'tinaye', complete: false, index: 0 },
+  ];
+  expect(deleteAll()).toEqual(arr);
+});
