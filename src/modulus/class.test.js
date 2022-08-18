a4197df6085dec89fd299f0081c78e3cff3ea804
tinayetsina <tinayetsina@outlook.com>
2022-08-18 15:49:37 +0800

    update - added describe function to test

--- src/modulus/class.test.js
+++ src/modulus/class.test.js
@@ -1,15 +1,22 @@
 // import {File} from './class.js';
 import { remove, add } from './mocks.js';
 
-test('add to list works', () => {
-  const input = 'Hello there';
-  expect(add(input)).toMatch('Hello there');
-});
+describe('Add/Remove tests', () => {
+  test('add to list works', () => {
+    const input = 'Hello there';
+    expect(add(input)).toMatch('Hello there');
+  });
+  test('multiple adds', () => {
+    expect(add('Input 1')).toMatch('Input 1');
+    expect(add('Input 2')).toMatch('Input 2');
+    expect(add('Input 3')).toMatch('Input 3');
+  });
 
-//
-test('remove from list works', () => {
-  const arr = [
-    { description: 'marsela', complete: false, index: 0 },
-  ];
-  expect(remove(0)).toEqual(arr);
+  //
+  test('remove from list works', () => {
+    const arr = [
+      { description: 'marsela', complete: false, index: 0 },
+    ];
+    expect(remove(0)).toEqual(arr);
+  });
 });
