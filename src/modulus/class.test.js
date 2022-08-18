b5e94d61a3747c4e8db79a9a8bcd035389508c32
tinayetsina <tinayetsina@outlook.com>
2022-08-18 16:02:05 +0800

    update - coverage added

--- src/modulus/class.test.js
+++ src/modulus/class.test.js
@@ -6,12 +6,6 @@ describe('Add/Remove tests', () => {
     const input = 'Hello there';
     expect(add(input)).toMatch('Hello there');
   });
-  test('multiple adds', () => {
-    expect(add('Input 1')).toMatch('Input 1');
-    expect(add('Input 2')).toMatch('Input 2');
-    expect(add('Input 3')).toMatch('Input 3');
-  });
-
   //
   test('remove from list works', () => {
     const arr = [
