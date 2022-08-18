17df3f8cf92905c83c1f272d0f6484b40022bb1b
tinayetsina <tinayetsina@outlook.com>
2022-08-18 21:28:12 +0800

    update - descriptions for edited completed

--- src/__test__/class.test.js
+++ src/__test__/class.test.js
@@ -14,31 +14,29 @@ describe('Add/Remove tests', () => {
   });
 });
 
-
-
-
-test('edit list', () => {
-  const arr = [
-    { description: 'CHANGED', complete: false, index: 0 },
-    { description: 'marsela', complete: false, index: 1 },
-  ];
-  const descr = 'CHANGED';
-  const index = 0;
-  expect(edit(descr, index)).toEqual(arr);
-});
-test('update the complete status', () => {
-  const arr = [
-    { description: 'suzana', complete: true, index: 0 },
-    { description: 'marsela', complete: false, index: 1 },
-  ];
-  document.body.innerHTML = '<input type="text" class="input">';
-  const input = document.querySelector('.input');
-  expect(complete(true, 0, input)).toEqual(arr);
-});
-
-test('clear all the true status lists', () => {
-  const arr = [
-    { description: 'tinaye', complete: false, index: 0 },
-  ];
-  expect(deleteAll()).toEqual(arr);
+describe('Editing methods test', () => {
+  test('edit list', () => {
+    const arr = [
+      { description: 'CHANGED', complete: false, index: 0 },
+      { description: 'marsela', complete: false, index: 1 },
+    ];
+    const descr = 'CHANGED';
+    const index = 0;
+    expect(edit(descr, index)).toEqual(arr);
+  });
+  test('update the complete status', () => {
+    const arr = [
+      { description: 'suzana', complete: true, index: 0 },
+      { description: 'marsela', complete: false, index: 1 },
+    ];
+    document.body.innerHTML = '<input type="text" class="input">';
+    const input = document.querySelector('.input');
+    expect(complete(true, 0, input)).toEqual(arr);
+  });
+  test('clear all the true status lists', () => {
+    const arr = [
+      { description: 'tinaye', complete: false, index: 0 },
+    ];
+    expect(deleteAll()).toEqual(arr);
+  });
 });
