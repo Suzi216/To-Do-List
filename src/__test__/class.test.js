46c608109324c60021e885aefb32b87877c954b5
tinayetsina <tinayetsina@outlook.com>
2022-08-18 16:13:04 +0800

    update - merger conflicts resolved

--- src/__test__/class.test.js
+++ src/__test__/class.test.js
@@ -1,31 +1,15 @@
-
 import { remove, add } from './mocks.js';
-import { File } from '../modulus/class.js'
 
-<<<<<<< HEAD:src/modulus/class.test.js
 describe('Add/Remove tests', () => {
   test('add to list works', () => {
     const input = 'Hello there';
     expect(add(input)).toMatch('Hello there');
   });
-  //
+
   test('remove from list works', () => {
     const arr = [
       { description: 'marsela', complete: false, index: 0 },
     ];
     expect(remove(0)).toEqual(arr);
   });
-=======
-test('add to list works', () => {
-  const input = 'Hello there';
-  expect(add(input)).toMatch('Hello there');
-});
-
-
-test('remove from list works', () => {
-  const arr = [
-    { description: 'marsela', complete: false, index: 0 },
-  ];
-  expect(remove(0)).toEqual(arr);
->>>>>>> 4f8f3b618450b49f538fd9b0ccb3591081c597ce:src/__test__/class.test.js
 });
